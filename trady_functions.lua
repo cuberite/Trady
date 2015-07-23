91e25631cdd925f3b1aff3ce248c66c604efd0e1
tonibm19 <tonibm19@gmail.com>
2015-07-23 17:52:07 +0200

    Updated plugin
    Also removed handy requeriment

--- trady_functions.lua
+++ trady_functions.lua
@@ -108,7 +108,7 @@ function CheckIntegrity( inWorld, inX, inY, inZ )			-- check on SIGN placement
 		end
 	end
 	inWorld:DoWithChestAt( inX, inY - 1, inZ, TestChest )
-	local sign = inWorld:GetSignLines( inX, inY, inZ, _line1, _line2, _line3, _line4 )
+	local sign = inWorld:GetSignLines( inX, inY, inZ)
 	if( sign ~= false ) then
 		foundSign = true
 	end
@@ -152,7 +152,7 @@ end
 function GetShopDescription( inWorld, inX, inY, inZ )
 	local _adress = GetAdress( inWorld, inX, inY, inZ )
 	if( ShopsData[_adress] ~= nil ) then
-		return (HANDY:Call( "PluralItemName", ShopsData[_adress].item, 10 ).." @["..inX.."; "..inY.."; "..inZ.."] in "..ShopsData[_adress].world)
+		return (PluralItemName(ShopsData[_adress].item, 10 ).." @["..inX.."; "..inY.."; "..inZ.."] in "..ShopsData[_adress].world)
 	end
 	return "no shop found"
 end
@@ -244,7 +244,7 @@ end
 --/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
 function BuyFromShop( inWorld, inPlayer, inX, inY, inZ )
 	local OperateChest = function( Chest )
-		local _c_balance, _c_free_space = HANDY:Call( "ReadChestForItem", Chest, ShopsData[_adress].item )
+		local _c_balance, _c_free_space = ReadChestForItem(Chest, ShopsData[_adress].item )
 		_trade_count = math.min( _trade_count, _c_balance )
 		if( _trade_count < ShopsData[_adress].amount ) then
 			if( _fractional_trade == false
@@ -252,7 +252,7 @@ function BuyFromShop( inWorld, inPlayer, inX, inY, inZ )
 				return 0
 			end
 		end
-		HANDY:Call( "TakeItemsFromChest", Chest, ShopsData[_adress].item, _trade_count )
+		TakeItemsFromChest(Chest, ShopsData[_adress].item, _trade_count)
 	end
 	_adress = GetAdress( inWorld, inX, inY, inZ )
 	local _result = 0	-- will contain amount of traded items
@@ -302,7 +302,7 @@ function BuyFromShop( inWorld, inPlayer, inX, inY, inZ )
 				end
 			end
 			-- 1.1 Also check how much items player could hold!
-			_p_balance, _p_free_space = HANDY:Call( "ReadPlayerForItem", inPlayer, ShopsData[_adress].item )
+			_p_balance, _p_free_space = ReadPlayerForItem(inPlayer, ShopsData[_adress].item)
 			_trade_count = math.min( _p_free_space, _trade_count )
 			if( _trade_count < ShopsData[_adress].amount ) then
 				OperationState.partial = true
@@ -331,7 +331,7 @@ function BuyFromShop( inWorld, inPlayer, inX, inY, inZ )
 			
 			-- 4. Add items to player.
 			_result = _trade_count
-			HANDY:Call( "GiveItemsToPlayer", inPlayer, ShopsData[_adress].item, _trade_count )
+			GiveItemsToPlayer(inPlayer, ShopsData[_adress].item, _trade_count )
 			OperationState.success = true
 			OperationState.amount = _result
 			OperationState.money_amount = _result*_unit_price
@@ -343,7 +343,7 @@ function BuyFromShop( inWorld, inPlayer, inX, inY, inZ )
 end
 function SellToShop( inWorld, inPlayer, inX, inY, inZ )
 	local OperateChest = function( Chest )
-		local _c_balance, _c_free_space = HANDY:Call( "ReadChestForItem", Chest, ShopsData[_adress].item )
+		local _c_balance, _c_free_space = ReadChestForItem(Chest, ShopsData[_adress].item)
 		_trade_count = math.min( _trade_count, _c_free_space )
 		LOGWARN( "Free space: ".._c_free_space )
 		if( _trade_count < ShopsData[_adress].amount ) then
@@ -352,7 +352,7 @@ function SellToShop( inWorld, inPlayer, inX, inY, inZ )
 				return 0
 			end
 		end
-		HANDY:Call( "PutItemsToChest", Chest, ShopsData[_adress].item, _trade_count )
+		PutItemsToChest(Chest, ShopsData[_adress].item, _trade_count)
 	end
 	_adress = GetAdress( inWorld, inX, inY, inZ )
 	local _result = 0	-- will contain amount of traded items
@@ -402,7 +402,7 @@ function SellToShop( inWorld, inPlayer, inX, inY, inZ )
 				end
 			end
 			-- 1.1 Also check how much items player could sell!
-			_p_balance, _p_free_space = HANDY:Call( "ReadPlayerForItem", inPlayer, ShopsData[_adress].item )
+			_p_balance, _p_free_space = ReadPlayerForItem(inPlayer, ShopsData[_adress].item)
 			_trade_count = math.min( _p_balance, _trade_count )
 			if( _trade_count < ShopsData[_adress].amount ) then
 				OperationState.partial = true
@@ -432,7 +432,7 @@ function SellToShop( inWorld, inPlayer, inX, inY, inZ )
 			
 			-- 4. Remove items from player.
 			_result = _trade_count
-			HANDY:Call( "TakeItemsFromPlayer", inPlayer, ShopsData[_adress].item, _trade_count )
+			TakeItemsFromPlayer(inPlayer, ShopsData[_adress].item, _trade_count)
 			OperationState.success = true
 			OperationState.amount = _result
 			OperationState.money_amount = _result *_unit_price
@@ -445,14 +445,15 @@ end
 --\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \
 --///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function GetMerchantTradeData( inMerchantName )
+	print(inMerchantName)
 	local CheckCashMachine = function( Chest )
-		_balance, _free_space = HANDY:Call( "ReadChestForItem", Chest, Settings.BarterItem )
+		_balance, _free_space = ReadChestForItem(Chest, Settings.BarterItem)
 	end
 	_balance = 0
 	_free_space = 0
 	
 	if( Settings.Barter == false ) then
-		_balance = COINY:Call( "GetMoney", inMerchantName )
+		_balance = PM:CallPlugin("Coiny", "getBalanceByName", inMerchantName)
 		_free_space = -1	-- makes no sense, but still...
 	else
 		if( TradersData[inMerchantName] ~= nil ) then
@@ -471,30 +472,30 @@ end
 function GetPlayerTradeData( inPlayerName )
 	_balance = 0
 	_free_space = 0
-	local _player = HANDY:Call( "GetPlayerByName", inPlayerName )
+	local _player = GetPlayerByName(inPlayerName)
 	if( Settings.Barter == false ) then
-		_balance = COINY:Call( "GetMoney", inPlayerName )
+		_balance = PM:CallPlugin("Coiny", "getBalanceByName", inPlayerName)
 		_free_space = -1	-- makes no sense, but still...
 	else
-		_balance, _free_space = HANDY:Call( "ReadPlayerForItem", _player, Settings.BarterItem )
+		_balance, _free_space = ReadPlayerForItem(_player, Settings.BarterItem)
 	end
 	return _balance, _free_space
 end
 
 function MakeTransaction( inPlayerName, inMerchantName, inAmount, inOperationFromChest )	-- UNSAFE, CHECK FIRST
 	if( Settings.Barter == false ) then
-		COINY:Call( "TransferMoney", inPlayerName, inMerchantName, tonumber( inAmount ) )
+		PM:CallPlugin("Coiny", "transferMoneyByName", inPlayerName, inMerchantName, inAmount )
 	else
 		-- coins, coins everywhere!
 		local OperateCashMachine = function( Chest )
 			if( inOperationFromChest == true ) then
-				HANDY:Call( "PutItemsToChest", Chest, Settings.BarterItem, inAmount )
+				PutItemsToChest(Chest, Settings.BarterItem, inAmount)
 			else
-				HANDY:Call( "TakeItemsFromChest", Chest, Settings.BarterItem, inAmount )
+				TakeItemsFromChest(Chest, Settings.BarterItem, inAmount)
 			end
 		end
-		local _player = HANDY:Call( "GetPlayerByName", inPlayerName )
-		local _merchant = HANDY:Call( "GetPlayerByName", inMerchantName )
+		local _player = GetPlayerByName(inPlayerName)
+		local _merchant = GetPlayerByName(inMerchantName)
 		
 		-- 1. Make operations with cash machine
 		local _x = TradersData[inMerchantName].cashmachine.x
@@ -505,16 +506,16 @@ function MakeTransaction( inPlayerName, inMerchantName, inAmount, inOperationFro
 		
 		-- 2. Make operations with pockets
 		if( inOperationFromChest == true ) then
-			HANDY:Call( "TakeItemsFromPlayer", _player, Settings.BarterItem, inAmount )
+			TakeItemsFromPlayer(_player, Settings.BarterItem, inAmount)
 		else
-			HANDY:Call( "GiveItemsToPlayer", _player, Settings.BarterItem, inAmount )
+			GiveItemsToPlayer(_player, Settings.BarterItem, inAmount)
 		end
 	end
 end
 --|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 function LoadData()
 	local _split = ""
-	file = io.open( PLUGIN:GetLocalDirectory().."/trady_shops.dat", "r" )
+	file = io.open( PLUGIN:GetLocalFolder().."/trady_shops.dat", "r" )
 	if( file == nil ) then		return 1	end
 	for line in file:lines() do
 		_split = LineSplit( line, ":" )
@@ -538,7 +539,7 @@ function LoadData()
 	end
 	file:close()
 	-- / / / / / / / / / / / /
-	file = io.open( PLUGIN:GetLocalDirectory().."/trady_merchants.dat", "r" )
+	file = io.open( PLUGIN:GetLocalFolder().."/trady_merchants.dat", "r" )
 	if( file == nil ) then		return 1	end
 	for line in file:lines() do
 		_split = LineSplit( line, ":" )
@@ -561,7 +562,7 @@ function LoadData()
 end
 function SaveData()
 	local line = ""
-	file = io.open( PLUGIN:GetLocalDirectory().."/trady_shops.dat", "w" )
+	file = io.open( PLUGIN:GetLocalFolder().."/trady_shops.dat", "w" )
 	for k,v in pairs( ShopsData ) do
 		line = ""..v.ownername
 		line = line..":"..v.world
@@ -575,7 +576,7 @@ function SaveData()
 	end
 	file:close()
 	-- / / / / / / / / / / / /
-	file = io.open( PLUGIN:GetLocalDirectory().."/trady_merchants.dat", "w" )
+	file = io.open( PLUGIN:GetLocalFolder().."/trady_merchants.dat", "w" )
 	for k,v in pairs( TradersData ) do
 		if( v.cashmachine ~= nil ) then
 			line = ""..k
@@ -590,7 +591,7 @@ end
 -- * * * * *
 function LoadSettings()
 	iniFile = cIniFile()
-	iniFile:ReadFile( PLUGIN:GetLocalDirectory().."/trady_settings.ini" )
+	iniFile:ReadFile( PLUGIN:GetLocalFolder().."/trady_settings.ini" )
 	local saveMode = iniFile:GetValueSet( "Settings", "SaveMode", "Timed" )
 	local barterItem = iniFile:GetValueSet( "Settings", "BarterItem", ItemTypeToString( E_ITEM_GOLD_NUGGET ) )
 	if( saveMode == "Timed" )		then Settings.SaveMode = eSaveMode_Timed	end
@@ -605,11 +606,11 @@ function LoadSettings()
 	LOG( "Ini reading on HaltSelf: "..tostring(Settings.HaltSelfTrade) )
 	Settings.UsingProtection = 		iniFile:GetValueSetB( "Settings", "UsingProtection", 	true )
 	Settings.BreakingProtection = 	iniFile:GetValueSetB( "Settings", "BreakingProtection",true )
-	iniFile:WriteFile( PLUGIN:GetLocalDirectory().."/trady_settings.ini" )
+	iniFile:WriteFile( PLUGIN:GetLocalFolder().."/trady_settings.ini" )
 end
 function SaveSettings()
 	iniFile = cIniFile()
-	iniFile:ReadFile( PLUGIN:GetLocalDirectory().."/trady_settings.ini" )
+	iniFile:ReadFile( PLUGIN:GetLocalFolder().."/trady_settings.ini" )
 	local saveMode = iniFile:GetValueSet( "Settings", "SaveMode", "Timed" )
 	local _barter_item = iniFile:GetValueSet( "Settings", "BarterItem", ItemTypeToString( E_ITEM_GOLD_NUGGET ) )
 	if( Settings.SaveMode == eSaveMode_Timed )		then	saveMode = "Timed"		end
@@ -624,7 +625,7 @@ function SaveSettings()
 	iniFile:SetValueB( "Settings", "HaltSelfTrade", 		Settings.HaltSelfTrade, 				false )
 	iniFile:SetValueB( "Settings", "UsingProtection", 		Settings.UsingProtection, 				false )
 	iniFile:SetValueB( "Settings", "BreakingProtection", 	Settings.BreakingProtection, 			false )
-	iniFile:WriteFile( PLUGIN:GetLocalDirectory().."/trady_settings.ini" )
+	iniFile:WriteFile( PLUGIN:GetLocalFolder().."/trady_settings.ini" )
 end
 --|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 -- splits line by any desired symbol
@@ -661,6 +662,209 @@ function StringToBool( inString )
 end
 
 
+--Functions exported from handy--
+
+function GetChestHeightCheat( inChest )
+	local chestGrid = inChest:GetContents()
+	LOGINFO( "This function serves no purpose now! You should consider chest:GetContents():GetHeight() now!" )
+	LOGINFO( "Also, you might find Handy's new 'IsChestDouble()' useful for your case" )
+	return chestGrid:GetHeight()
+end
+function IsChestDouble( inChest )
+	local chestHeight = inChest:GetContents():GetHeight()
+	if( chestHeight == 3 ) then
+		return false
+	end
+	return true
+end
+-- Those two checks how many items of given inItemID chest and player have, and how much they could fit inside them
+function ReadChestForItem( inChest, inItemID )
+	return ReadGridForItems( inChest:GetContents(), inItemID )
+end
+function ReadPlayerForItem( inPlayer, inItemID )
+	local inventoryFound, inventoryFree = ReadGridForItems( inPlayer:GetInventory():GetInventoryGrid(), inItemID )
+	local hotbarFound, hotbarFree = ReadGridForItems( inPlayer:GetInventory():GetHotbarGrid(), inItemID )
+	local itemsFound = inventoryFound + hotbarFound
+	local freeSpace = inventoryFree + hotbarFree
+	return itemsFound, freeSpace
+end
+-- Following functions are for chest-related operations
+-- BEWARE! Those assume you did checked if chest has items/space in it!
+function ReadGridForItems( inGrid, inItemID )
+	local itemsFound = 0
+	local freeSpace = 0
+	local slotsCount = inGrid:GetNumSlots()
+	local testerItem = cItem( inItemID )
+	local maxStackSize = testerItem:GetMaxStackSize()
+	for index = 0, (slotsCount - 1) do
+		slotItem = inGrid:GetSlot( index )
+		if( slotItem:IsEmpty() ) then
+			freeSpace = freeSpace + maxStackSize
+		else
+			if( slotItem:IsEqual( testerItem ) ) then
+				itemsFound = itemsFound + slotItem.m_ItemCount
+				freeSpace = maxStackSize - slotItem.m_ItemCount
+			end
+		end
+	end
+	return itemsFound, freeSpace
+end
+
+function TakeItemsFromGrid( inGrid, inItem )
+	local slotsCount = inGrid:GetNumSlots()
+	local removedItem = cItem( inItem )
+	for index = 0, (slotsCount - 1) do
+		slotItem = inGrid:GetSlot( index )
+		if( slotItem:IsSameType( removedItem ) ) then
+			if( slotItem.m_ItemCount <= removedItem.m_ItemCount ) then
+				removedItem.m_ItemCount = removedItem.m_ItemCount - slotItem.m_ItemCount
+				inGrid:EmptySlot( index )
+			else
+				removedItem.m_ItemCount = slotItem.m_ItemCount - removedItem.m_ItemCount
+				inGrid:SetSlot( index, removedItem )
+				removedItem.m_ItemCount = 0
+			end
+			if( removedItem.m_ItemCount <= 0 ) then		break	end
+		end
+	end
+	return removedItem.m_ItemCount
+end
+--------------
+function TakeItemsFromChest( inChest, inItemID, inAmount )	-- MIGHT BE UNSAFE! CHECK FOR ITEMS FIRST!!
+	local chestGrid = inChest:GetContents()
+	local removedItem = cItem( inItemID, inAmount )
+	TakeItemsFromGrid( chestGrid, removedItem )
+end
+function PutItemsToChest( inChest, inItemID, inAmount )
+	local chestGrid = inChest:GetContents()
+	local addedItem = cItem( inItemID, inAmount )
+	chestGrid:AddItem( addedItem )
+end
+-- Similar to chest-related.
+function TakeItemsFromPlayer( inPlayer, inItemID, inAmount )	-- MIGHT BE UNSAFE! CHECK FIRST!
+	local removedItem = cItem( inItemID, inAmount )
+	local inventoryGrid = inPlayer:GetInventory():GetInventoryGrid()
+	local hotbarGrid = inPlayer:GetInventory():GetHotbarGrid()
+	local itemsLeft = TakeItemsFromGrid( inventoryGrid, removedItem )
+	if( itemsLeft > 0 ) then
+		removedItem = cItem( inItemID, itemsLeft )
+		TakeItemsFromGrid( hotbarGrid, removedItem )
+	end
+end
+function GiveItemsToPlayer( inPlayer, inItemID, inAmount )
+	local addedItem = cItem( inItemID, inAmount )
+	local inventoryGrid = inPlayer:GetInventory():GetInventoryGrid()
+	local hotbarGrid = inPlayer:GetInventory():GetHotbarGrid()
+	local itemsAdded = inventoryGrid:AddItem( addedItem )
+	if( itemsAdded < inAmount ) then
+		addedItem.m_ItemCount = addedItem.m_ItemCount - itemsAdded
+		hotbarGrid:AddItem( addedItem )
+	end
+end
+-- This function returns item max stack for a given itemID. It uses vanilla max stack size, and uses several non-common items notations;
+-- Those are:
+-- oneonerecord( because aparently 11record wasn't the best idea in lua scripting application )
+-- carrotonastick( because it wasn't added to items.txt yet )
+-- waitrecord( for same reason )
+-- Feel free to ignore the difference, or to add those to items.txt
+function GetItemMaxStack( inItemID )
+	local testerItem = cItem( inItemID )
+	LOGINFO( "This function serves no real purpose now, maybe consider using cItem:GetMaxStackSize()?" )
+	return testerItem:GetMaxStackSize()
+end
+function ItemIsArmor( inItemID, inCheckForHorseArmor )
+	inCheckForHorseArmor = inCheckForHorseArmor or false
+	if( inItemID == E_ITEM_LEATHER_CAP )		then	return true		end
+	if( inItemID == E_ITEM_LEATHER_TUNIC )		then	return true		end
+	if( inItemID == E_ITEM_LEATHER_PANTS )		then	return true		end
+	if( inItemID == E_ITEM_LEATHER_BOOTS )		then	return true		end
+	
+	if( inItemID == E_ITEM_CHAIN_HELMET )		then	return true		end
+	if( inItemID == E_ITEM_CHAIN_CHESTPLATE )	then	return true		end
+	if( inItemID == E_ITEM_CHAIN_LEGGINGS )		then	return true		end
+	if( inItemID == E_ITEM_CHAIN_BOOTS )		then	return true		end
+	
+	if( inItemID == E_ITEM_IRON_HELMET )		then	return true		end
+	if( inItemID == E_ITEM_IRON_CHESTPLATE )	then	return true		end
+	if( inItemID == E_ITEM_IRON_LEGGINGS )		then	return true		end
+	if( inItemID == E_ITEM_IRON_BOOTS )			then	return true		end
+	
+	if( inItemID == E_ITEM_DIAMOND_HELMET )		then	return true		end
+	if( inItemID == E_ITEM_DIAMOND_CHESTPLATE )	then	return true		end
+	if( inItemID == E_ITEM_DIAMOND_LEGGINGS )	then	return true		end
+	if( inItemID == E_ITEM_DIAMOND_BOOTS )		then	return true		end
+	
+	if( inItemID == E_ITEM_GOLD_HELMET )		then	return true		end
+	if( inItemID == E_ITEM_GOLD_CHESTPLATE )	then	return true		end
+	if( inItemID == E_ITEM_GOLD_LEGGINGS )		then	return true		end
+	if( inItemID == E_ITEM_GOLD_BOOTS )			then	return true		end
+	
+	if( inCheckForHorseArmor ) then
+		if( inItemID == E_ITEM_IRON_HORSE_ARMOR )		then	return true		end
+		if( inItemID == E_ITEM_GOLD_HORSE_ARMOR )		then	return true		end
+		if( inItemID == E_ITEM_DIAMOND_HORSE_ARMOR )	then	return true		end
+	end
+	return false
+end
+-- Returns full-length playername for a short name( usefull for parsing commands )
+function GetExactPlayername( inPlayerName )
+	local _result = inPlayerName
+	local function SetProcessingPlayername( inPlayer )
+		_result = inPlayer:GetName()
+	end
+	cRoot:Get():FindAndDoWithPlayer( inPlayerName, SetProcessingPlayername )
+	return _result
+end
+function GetPlayerByName( inPlayerName )
+	local _player
+	local PlayerSetter = function( Player )
+		_player = Player
+	end
+	cRoot:Get():FindAndDoWithPlayer( inPlayerName, PlayerSetter )
+	return _player
+end
+--[[
+Not-so-usual math _functions
+]]
+-- Rounds floating point number. Because lua guys think this function doesn't deserve to be presented in lua's math
+function round( inX )
+  if( inX%2 ~= 0.5 ) then
+    return math.floor( inX + 0.5 )
+  end
+  return inX - 0.5
+end
+--[[
+Functions I use for filework and stringswork
+]]
+function PluralString( inValue, inSingularString, inPluralString )
+	local _value_string = tostring( inValue )
+	if( _value_string[#_value_string] == "1" ) then
+		return inSingularString
+	end
+	return inPluralString
+end
+function PluralItemName( inItemID, inAmount )	-- BEWARE! TEMPORAL SOLUTION THERE! :D
+	local _value_string = tostring( inValue )
+	local _name = ""
+	if( _value_string[#_value_string] == "1" ) then
+		-- singular names
+		_name = ItemTypeToString( inItemID )
+	else
+		-- plural names
+		_name = ItemTypeToString( inItemID ).."s"
+	end
+	return _name
+end
+-- for filewriting purposes. 0 = false, 1 = true
+function StringToBool( inValue )
+	if( inValue == "1" ) then return true end
+	return false
+end
+-- same, but reversal
+function BoolToString( inValue )
+	if( inValue == true ) then return 1 end
+	return 0
+end
 
 
 
